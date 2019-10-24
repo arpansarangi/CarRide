23684ec972ae987eb5425c4fecb96e9804343021
samael042 <44134245+samael042@users.noreply.github.com>
2019-10-25 00:55:19 +0530

    customer.java
    This stores all of the details that is needed for a customer to ride. Constructor which takes appropriate arguments  and all of the content has been made private for security purpose.

---
+++ customer.java
@@ -0,0 +1,26 @@
+//IMPORT PROPER PACKAGES LATER
+package com.mycompany.arabella;
+
+public class customer {
+    private String name;
+    private int id;
+    private String currentLocation;
+    private String destination;
+    private String userName;
+    private String password;
+    private String phoneNumber;
+    private String emailID;
+    private int wallet;
+    //COMMENT THIS SHIT LATER   
+    customer(String name, int id, String currentLocation, String destination, String userName, String password, String phoneNumber, String emailID, int wallet)
+    {
+        this.name = name;
+        this.id = id;
+        this.currentLocation = currentLocation;
+        this.destination = destination;
+        this.userName = userName;
+        this.password = password;
+        this.emailID = emailID;
+        this.wallet = wallet;
+    }
+}
