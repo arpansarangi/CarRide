9ee3ebd17b433c60fd0091fc312b4c7ca6101cdd
samael042 <44134245+samael042@users.noreply.github.com>
2019-10-25 00:51:57 +0530

    customer.java
    A class which stores details of the customer. All of the variables are declared as private in order to maintain proper security. An object corresponding to this class can be declared using the above Constructor.

---
+++ customer.java
@@ -0,0 +1,18 @@
+package com.mycompany.arabella;
+
+public class driver {
+    private String name;
+    private String phoneNumber;
+    private double rating; //assume it is out of 5
+    private String currentLocation;
+    private String license;
+    
+    driver(String name, String phoneNumer, double rating, String currentLocation, String license)
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.rating = rating;
+        this.currentLocation = currentLocation;
+        this.license = license;
+    }                    
+}
