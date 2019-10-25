f302deedda9f5470179dc9976e6ec3e7b373632e
samael042 <44134245+samael042@users.noreply.github.com>
2019-10-25 15:30:40 +0530

    Update customer.java

--- customer.java
+++ customer.java
@@ -24,14 +24,17 @@ public class customer {
         this.emailID = emailID;
         this.wallet = wallet;
     }
+    //Call to display the current balance
     int balance()
     {
         return wallet;
     }
+    //Call before booking a ride to check wether the customer has enough balance in his waller(300 Rs atleast)
     bool isEnough()
     {
         reteurn (wallet >= 300);   
     }
+    //Add money to the wallet
     void addBalance(int incr)
     {
         this.wallet += incr;
